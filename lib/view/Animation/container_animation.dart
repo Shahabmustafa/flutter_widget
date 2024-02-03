f50ee620b1aaa47421811e18c4a78e7684eb0bd7
Shahabmustafa <shahabmustafa57@gmail.com>
2024-02-03 22:59:52 +0500

    add container animation

---
+++ lib/view/Animation/container_animation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContainerAnimation extends StatefulWidget {
+  const ContainerAnimation({super.key});
+
+  @override
+  State<ContainerAnimation> createState() => _ContainerAnimationState();
+}
+
+class _ContainerAnimationState extends State<ContainerAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
